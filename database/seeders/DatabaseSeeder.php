9fa8c1c5e1c1424e844bd072fb9793915af1cb0e
Marc Geurts <marc.geurts@scarlet.be>
2023-04-29 16:48:37 +0200

    Installation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersSeeder::class);
         $this->call(CustomersSeeder::class);
-        $this->call(UserlogsSeeder::class);
+
+        //$this->call(UserlogsSeeder::class);
     }
 }
