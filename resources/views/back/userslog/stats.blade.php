e1250845f8ebb3d84ef8441a65ab464aa30a9095
Marc Geurts <marc.geurts@scarlet.be>
2023-04-29 16:54:59 +0200

    Installation

--- resources/views/back/userslog/stats.blade.php
+++ resources/views/back/userslog/stats.blade.php
@@ -5,25 +5,29 @@
 @endsection
 
 @section('content')
-    <div class="card mb-2">
-        <div class="card-header d-print-none">
-            <div class="row">
-                <div class="col">Users Statitics - Country of origin</div>
+    <div class="row">
+        <div class="col-md-6 offset-md-3">
+            <div class="card mb-2">
+                <div class="card-header d-print-none">
+                    <div class="row">
+                        <div class="col">Users Statitics - Country of origin</div>
 
-                <div class="col fs-5 text-end">
-                    <button type="button" class="btn btn-outline-secondary btn-sm me-2" title="Print" tabindex="-1" onclick="window.print();">
-                        <img src="{{ asset('img/icons/printer.png') }}" class="img-fluid" />
-                    </button>
-                    <img src="{{ asset('img/icons/statistics.png') }}" class="img-fluid" />
-                    <img src="{{ asset('img/icons/persons.png') }}" class="img-fluid" />
+                        <div class="col fs-5 text-end">
+                            <button type="button" class="btn btn-outline-secondary btn-sm me-2" title="Print" tabindex="-1" onclick="window.print();">
+                                <img src="{{ asset('img/icons/printer.png') }}" class="img-fluid" />
+                            </button>
+                            <img src="{{ asset('img/icons/statistics.png') }}" class="img-fluid" />
+                            <img src="{{ asset('img/icons/persons.png') }}" class="img-fluid" />
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
 
-        <div class="card-body">
-            <div class="row">
-                <div class="col-lg-10 offset-lg-1">
-                    <canvas id="myChart"></canvas>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-lg-10 offset-lg-1">
+                            <canvas id="myChart"></canvas>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -46,7 +50,7 @@
 
         const ctx = document.getElementById("myChart").getContext("2d");
         const myChart = new Chart(ctx, {
-            type: 'doughnut',
+            type: 'pie',
             data: data,
             plugins: {
                 legend: {
