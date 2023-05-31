05c4ff487e00abb95550b0fa244a7590743b6862
Marc Geurts <marc.geurts@scarlet.be>
2023-05-31 14:35:13 +0200

    Installation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Pagination\Paginator;
-use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 
@@ -25,11 +26,18 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrapFive();
 
-        // log all queries when in development
-        if (App::environment('local')) {
+        // log all queries when in not in production
+        if (! app()->isProduction()) {
             DB::listen(function ($query) {
                 logger(Str::replaceArray('?', $query->bindings, $query->sql));
             });
         }
+
+        // log all N+1 queries
+        Model::preventLazyLoading();
+
+        Model::handleLazyLoadingViolationUsing(function ($model, $relation) {
+            Log::warning("N+1 Query detected rr\r\n" . sprintf('N+1 Query detected in model %s on relation %s', get_class($model), $relation));
+        });
     }
 }
