164227be14d2d23cdfa8021517f45f0b3324ce54
Marc Geurts <marc.geurts@scarlet.be>
2023-06-06 20:25:39 +0200

    Installation

--- resources/views/back/components/offcanvas.blade.php
+++ resources/views/back/components/offcanvas.blade.php
@@ -1,10 +1,10 @@
-<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+<div class="offcanvas offcanvas-start text-bg-dark" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
     <div class="offcanvas-header p-1">
         <h6 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h6>
-        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
 
-    <div class="offcanvas-body bg-dark">
+    <div class="offcanvas-body">
         @include('back.components.offcanvas-menu')
     </div>
 </div>
