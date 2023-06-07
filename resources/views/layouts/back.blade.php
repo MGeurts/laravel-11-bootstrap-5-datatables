9d9c9bca2582d8f3fc45e7680c0214350971977a
Marc Geurts <marc.geurts@scarlet.be>
2023-06-07 08:57:46 +0200

    Installation

--- resources/views/layouts/back.blade.php
+++ resources/views/layouts/back.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-bs-theme="auto">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-bs-theme="light">
 
 <head>
     <meta charset="utf-8">
