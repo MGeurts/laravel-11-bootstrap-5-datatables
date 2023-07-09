023da5314db7c2420580e3baceef8e36fdc03a53
Marc Geurts <marc.geurts@scarlet.be>
2023-07-09 22:53:02 +0200

    Installation

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -68,7 +69,7 @@ class User extends Authenticatable
     /* -------------------------------------------------------------------------------------------- */
     // Relationships
     /* -------------------------------------------------------------------------------------------- */
-    public function userlogs()
+    public function userlogs(): HasMany
     {
         return $this->hasMany(Userlog::class);
     }
--- app/Models/Userlog.php
+++ app/Models/Userlog.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Userlog extends Model
 {
@@ -39,7 +40,7 @@ class Userlog extends Model
     /* -------------------------------------------------------------------------------------------- */
     // Relationships
     /* -------------------------------------------------------------------------------------------- */
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class)->withTrashed();
     }
