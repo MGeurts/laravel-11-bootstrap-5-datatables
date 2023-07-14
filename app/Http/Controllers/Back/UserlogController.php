cffde43ebd1d052973b1813f2e78a922e17b3610
Marc Geurts <marc.geurts@scarlet.be>
2023-07-14 12:35:29 +0200

    Installation

--- app/Http/Controllers/Back/UserlogController.php
+++ app/Http/Controllers/Back/UserlogController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers\Back;
 
+use App\Http\Controllers\Controller;
 use App\Models\Userlog;
 use Illuminate\Support\Carbon;
-use Khill\Lavacharts\Lavacharts;
-use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Gate;
+use Khill\Lavacharts\Lavacharts;
 use Symfony\Component\HttpFoundation\Response;
 
 class UserlogController extends Controller
@@ -40,7 +40,7 @@ class UserlogController extends Controller
 
         $data['chart_data'] = json_encode([
             'label' => $statistics->pluck('country_name'),
-            'data' => $statistics->pluck('visitors')
+            'data' => $statistics->pluck('visitors'),
         ]);
 
         return view('back.userslog.stats-country-pie', $data);
@@ -52,7 +52,7 @@ class UserlogController extends Controller
 
         $lava = new Lavacharts([
             // This is a fake Google API key, replace it with your own legal Google API key !!
-            'maps_api_key' => '23ay5t987354inr28m9crg893crgt9arc98tr2a896tarc2896ta28'
+            'maps_api_key' => '23ay5t987354inr28m9crg893crgt9arc98tr2a896tarc2896ta28',
         ]);
 
         $visitors = $lava->DataTable();
@@ -70,17 +70,17 @@ class UserlogController extends Controller
             ->addRows($data);
 
         $lava->GeoChart('Visitors', $visitors, [
-            'colorAxis'                 => ['minValue' => 0,  'colors' => ['#FF0000', '#00FF00']],   //ColorAxis Options
-            'datalessRegionColor'       => '#d0d0d0',
-            'displayMode'               => 'auto',
+            'colorAxis' => ['minValue' => 0,  'colors' => ['#FF0000', '#00FF00']],   //ColorAxis Options
+            'datalessRegionColor' => '#d0d0d0',
+            'displayMode' => 'auto',
             'enableRegionInteractivity' => true,
-            'keepAspectRatio'           => true,
-            'region'                    => 'world',
-            'magnifyingGlass'           => ['enable' => true, 'zoomFactor' => 7.5],    //MagnifyingGlass Options
-            'markerOpacity'             => 1.0,
-            'resolution'                => 'countries',
-            'sizeAxis'                  => null,
-            'backgroundColor'           => '#f0f0f0',
+            'keepAspectRatio' => true,
+            'region' => 'world',
+            'magnifyingGlass' => ['enable' => true, 'zoomFactor' => 7.5],    //MagnifyingGlass Options
+            'markerOpacity' => 1.0,
+            'resolution' => 'countries',
+            'sizeAxis' => null,
+            'backgroundColor' => '#f0f0f0',
         ]);
 
         return view('back.userslog.stats-country-map', compact('lava'));
@@ -120,7 +120,7 @@ class UserlogController extends Controller
 
         $data['chart_data'] = json_encode([
             'label' => $statistics->pluck('period'),
-            'data' => $statistics->pluck('visitors')
+            'data' => $statistics->pluck('visitors'),
         ]);
 
         return view('back.userslog.stats-periode', $data);
