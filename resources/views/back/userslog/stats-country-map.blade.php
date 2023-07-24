386a969bd1e508c588ad4d62f6da22da34df35ce
Marc Geurts <marc.geurts@scarlet.be>
2023-07-24 18:49:55 +0200

    Installation

--- resources/views/back/userslog/stats-country-map.blade.php
+++ resources/views/back/userslog/stats-country-map.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
     <div class="row">
-        <div class="col-md-8 offset-md-2">
+        <div class="col-xxl-8 offset-xxl-2">
             <div class="card mb-2">
                 <div class="card-header d-print-none">
                     <div class="row">
