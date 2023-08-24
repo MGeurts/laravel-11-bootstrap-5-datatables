cb546f7679e46c6b432ffac812bcbc8dd17e8c1d
Marc Geurts <marc.geurts@scarlet.be>
2023-08-24 19:25:26 +0200

    Installation

--- resources/views/back/developer/impressum.blade.php
+++ resources/views/back/developer/impressum.blade.php
@@ -32,7 +32,7 @@
                             </ul>
 
                             <ul>
-                                <li><a target="_blank" href="https://github.com/barryvdh/laravel-debugbar/">barryvdh/laravel-debugbar</a> - 3.8.2</li>
+                                <li><a target="_blank" href="https://github.com/barryvdh/laravel-debugbar/">barryvdh/laravel-debugbar</a> - 3.9.1</li>
                             </ul>
 
                             <ul>
