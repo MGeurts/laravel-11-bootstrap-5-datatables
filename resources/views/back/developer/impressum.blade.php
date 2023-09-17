1dfbd1dbe2f0173f2b53bffb6629986a1396996f
Marc Geurts <marc.geurts@scarlet.be>
2023-09-17 22:51:13 +0200

    Installation

--- resources/views/back/developer/impressum.blade.php
+++ resources/views/back/developer/impressum.blade.php
@@ -49,7 +49,7 @@
 
                             <ul>
                                 <li><a target="_blank" href="https://getbootstrap.com/">Bootstrap</a> - 5.3.2</li>
-                                <li><a target="_blank" href="https://icons.getbootstrap.com//">Bootstrap Icons</a> - 1.11.0</li>
+                                <li><a target="_blank" href="https://icons.getbootstrap.com//">Bootstrap Icons</a> - 1.11.1</li>
                                 <li><a target="_blank" href="https://github.com/coliff/bootstrap-print-css/">Bootstrap Print CSS</a> - 1.0.1</li>
                             </ul>
 
