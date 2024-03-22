c14092e7920b2ec1cf2c8c0a0c6fd2f6001adfd5
kreaweb.be <webmaster@kreaweb.be>
2024-03-22 15:44:39 +0100

    Installation

---
+++ app/Http/Requests/CustomerUpdateRequest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class CustomerUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'customer_last_name' => [
+                'nullable',
+                'string',
+                'max:50',
+                'required_without_all:customer_first_name,company_name',
+            ],
+            'customer_first_name' => [
+                'nullable',
+                'string',
+                'max:50',
+                'required_without_all:customer_last_name,company_name',
+            ],
+            'company_name' => [
+                'nullable',
+                'string',
+                'max:50',
+                'required_without_all:customer_last_name,customer_first_name',
+            ],
+            'email' => [
+                'nullable',
+                'string',
+                'max:191',
+                'email',
+                Rule::unique('customers', 'email')->whereNull('deleted_at')->ignore($this->customer),
+            ],
+        ];
+    }
+}
