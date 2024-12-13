fc6b38504998ecb991e4e5366e5cb319b78890a1
MGeurts <5404867+MGeurts@users.noreply.github.com>
2024-12-13 17:39:58 +0100

    Impressum - Webserver (neutral)

--- resources/views/back/developer/impressum.blade.php
+++ resources/views/back/developer/impressum.blade.php
@@ -108,91 +108,8 @@
             </h2>
 
             <div id="collapseWebserver" class="accordion-collapse collapse" aria-labelledby="headingWebserver" data-bs-parent="#accordionImpressum">
-                <div class="accordion-body">
-                    <div class="row">
-                        <div class="col-md-7">
-                            <table width="100%">
-                                <tr>
-                                    <td colspan="4" class="text-center">
-                                        <a target="_blank" href="https://github.com/leokhoa/laragon/releases">
-                                            <img src="{{ asset('img/general/laragon.png') }}" alt="LARAGON" width="200px" />
-                                        </a>
-                                    </td>
-                                </tr>
-
-                                <tr>
-                                    <td colspan="4" class="text-center">6.0.0</td>
-                                </tr>
-                                <tr>
-                                    <td colspan="4"><br /><br /></td>
-                                </tr>
-
-                                <tr>
-                                    <td class="text-center">
-                                        <a target="_blank" href="http://httpd.apache.org/">
-                                            <img src="{{ asset('img/general/xampp-apache-001.png') }}" alt="Apache" />
-                                        </a>
-                                    </td>
-                                    <td class="text-center">
-                                        <a target="_blank" href="http://php.net/">
-                                            <img src="{{ asset('img/general/xampp-php-001.png') }}" alt="PHP" />
-                                        </a>
-                                    </td>
-                                    <td class="text-center">
-                                        <a target="_blank" href="https://dev.mysql.com/downloads/mysql/">
-                                            <img src="{{ asset('img/general/xampp-mysql-001.png') }}" alt="MySQL" />
-                                        </a>
-                                    </td>
-                                    <td class="text-center">
-                                        <a target="_blank" href="http://www.phpmyadmin.net/">
-                                            <img src="{{ asset('img/general/xampp-phpmyadmin-001.png') }}" alt="phpMyAdmin" />
-                                        </a>
-                                    </td>
-                                </tr>
-
-                                <tr>
-                                    <td colspan="4"></td>
-                                </tr>
-
-                                <tr>
-                                    <td class="text-center">
-                                        @if (config('app.env') == 'local')
-                                            {{ apache_get_version() }}
-                                        @else
-                                            ????
-                                        @endif
-                                    </td>
-                                    <td class="text-center">@php echo phpversion(); @endphp</td>
-                                    <td class="text-center">8.3.0</td>
-                                    <td class="text-center">5.2.1</td>
-                                </tr>
-
-                                <tr>
-                                    <td colspan="4"><br /><br /></td>
-                                </tr>
-
-                                <tr>
-                                    <td colspan="4" class="text-center">
-                                        <a target="_blank" href="https://jquery.com/">
-                                            <img src="{{ asset('img/general/jquery-001.png') }}" alt="jQuery" />
-                                        </a>
-                                    </td>
-                                </tr>
-
-                                <tr>
-                                    <td colspan="4" class="text-center jQuery_version"></td>
-                                </tr>
-
-                                <tr>
-                                    <td colspan="4"><br /><br /></td>
-                                </tr>
-                            </table>
-                        </div>
-
-                        <div class="col-md-5 text-center">
-                            <img src="{{ asset('img/general/datatables-serverside-processing.png') }}" class="img-fluid" alt="Datatables-SSP" />
-                        </div>
-                    </div>
+                <div class="accordion-body text-center">
+                    <img src="{{ asset('img/general/datatables-serverside-processing.png') }}" class="img-fluid" alt="Datatables-SSP" />
                 </div>
             </div>
         </div>
@@ -201,11 +118,3 @@
 
     <p><small>(*) Version numbers can be bumped up.</small></p>
 @endsection
-
-@push('scripts')
-    <script type="module">
-        /* -------------------------------------------------------------------------------------------- */
-        $(".jQuery_version").html(jQuery.fn.jquery);
-        /* -------------------------------------------------------------------------------------------- */
-    </script>
-@endpush
