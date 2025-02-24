d8b8c02a901b8d8c115668ee8fe36f2a080b09ec
MGeurts <5404867+MGeurts@users.noreply.github.com>
2025-02-24 10:12:20 +0100

    Removed HasFactory from Country model

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -3,13 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Casts\Attribute;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Country extends Model
 {
-    use HasFactory;
-
     protected $fillable = [
         'name',
         'iso2',
